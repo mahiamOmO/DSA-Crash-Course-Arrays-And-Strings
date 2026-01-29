b6cfb1dee537f902b85fc6012967ebef1f9f00fd
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2026-01-30 00:22:49 +0600

    intersection_approach

---
+++ DSA-Array-String/string/intersection_approach.py
@@ -0,0 +1,14 @@
+def intersection(a, b):
+    items_set = set(a)
+    return [ele for ele in b if ele in items_set]
+
+
+#for item in a:
+   # items.add(item)
+#print(items)
+
+#for ele in b:
+   # if ele in items_set:
+      #  result.append(ele)
+#return result
+#intersection([4,2,1,6],[3,6,9,2,10])
---
+++ DSA-Array-String/string/intersection_approach_readme.md
@@ -0,0 +1,6 @@
+[4,2,1,6]    [3,6,9,2,10]
+
+intersection [2,6]
+
+#time complx: 0(n*m)
+#Space: 0(min(n,m))
