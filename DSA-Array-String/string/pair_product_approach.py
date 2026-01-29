4e1c47284767a7e83adc19d41e9a477671c34b3a
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2026-01-29 15:36:11 +0600

    pair_product_code

---
+++ DSA-Array-String/string/pair_product_approach.py
@@ -0,0 +1,10 @@
+def pair_product(numbers, target_product):
+    previous_nums = {}
+
+    for index, num in enumerate(numbers):
+        complement = target_product / num
+
+        if complement in previous_nums:
+            return (index, previous_nums[complement])
+
+        previous_nums[num] = index
